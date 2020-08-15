613e865208828635e2797887c34e4895e315f32a
Mestkon <havhoi92@gmail.com>
2020-08-15 16:18:04 +0200

    core

---
+++ include/mtk/core.hpp
@@ -0,0 +1,21 @@
+//! @file
+//! Includes everything in mtk/core/
+
+//! @defgroup core mtk/core.hpp
+//! Includes everything in mtk/core/
+
+#include <mtk/core/allocator_traits.hpp>
+#include <mtk/core/assert.hpp>
+#include <mtk/core/casts.hpp>
+#include <mtk/core/container_traits.hpp>
+#include <mtk/core/guarded_ptr.hpp>
+#include <mtk/core/iterator_traits.hpp>
+#include <mtk/core/preprocessor.hpp>
+#include <mtk/core/scope_guard.hpp>
+#include <mtk/core/type_traits.hpp>
+#include <mtk/core/types.hpp>
+
+//! @brief Everything in mtkcore is contained
+//! within this namespace.
+namespace mtk {
+} // namespace mtk
