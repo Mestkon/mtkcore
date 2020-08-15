59368c4ca553ec5f13205c29a98f40778ef0578b
Mestkon <havhoi92@gmail.com>
2020-08-15 16:17:54 +0200

    global

---
+++ include/mtk/global.hpp
@@ -0,0 +1,11 @@
+//! @file
+//! Most important basic features of mtk/core
+
+//! @defgroup global mtk/global.hpp
+//! Includes `<mtk/core/assert.hpp>`, `<mtk/core/casts.hpp>`,
+//! `<mtk/core/preprocessor.hpp>`, and `<mtk/core/types.hpp>`.
+
+#include <mtk/core/assert.hpp>
+#include <mtk/core/casts.hpp>
+#include <mtk/core/preprocessor.hpp>
+#include <mtk/core/types.hpp>
