ae04ec4872220ee5f2704c8c78cf78c82b831ebf
Mestkon <havhoi92@gmail.com>
2020-12-30 10:15:15 +0100

    add the rest of the tests for the basic matrix_interface

--- tests/linalg/matrix.cpp
+++ tests/linalg/matrix.cpp
@@ -510,3 +510,144 @@ TEST_CASE("linalg/matrix: move assignment copies the given matrix", "[linalg]")
 {
 	invoke_for_each_type<test_move_assignment, DYNAMIC_TYPES>();
 }
+
+template<class Mat>
+struct test_row_iterator
+{
+	void
+	operator()() const
+	{
+		auto m = create_mat<Mat>();
+
+		auto r = m.rows();
+		auto c = m.columns();
+		auto row = m.begin_rows();
+		for (uint i = 0; i < r; ++i) {
+			auto it = row->begin();
+			for (uint j = 0; j < c; ++j) {
+				REQUIRE(*it == m[i][j]);
+				*(it++) = typename Mat::value_type();
+				REQUIRE(m[i][j] == typename Mat::value_type());
+			}
+			REQUIRE(it == row->end());
+			++row;
+		}
+		REQUIRE(row == m.end_rows());
+	}
+};
+
+TEST_CASE("linalg/matrix: row iterator iterates over the rows of the given matrix", "[linalg]")
+{
+	invoke_for_each_type<test_row_iterator, TYPES>();
+}
+
+template<class Mat>
+struct test_column_iterator
+{
+	void
+	operator()() const
+	{
+		auto m = create_mat<Mat>();
+
+		auto r = m.rows();
+		auto c = m.columns();
+		auto col = m.begin_columns();
+		for (uint j = 0; j < c; ++j) {
+			auto it = col->begin();
+			for (uint i = 0; i < r; ++i) {
+				REQUIRE(*it == m[i][j]);
+				*(it++) = typename Mat::value_type();
+				REQUIRE(m[i][j] == typename Mat::value_type());
+			}
+			REQUIRE(it == col->end());
+			++col;
+		}
+		REQUIRE(col == m.end_columns());
+	}
+};
+
+TEST_CASE("linalg/matrix: column iterator iterates over the columns of the given matrix", "[linalg]")
+{
+	invoke_for_each_type<test_column_iterator, TYPES>();
+}
+
+template<class Mat>
+struct test_rows
+{
+	void
+	operator()() const
+	{
+		auto m = create_mat<Mat>();
+
+		auto r = m.rows();
+		auto c = m.columns();
+
+		for (uint i = 0; i < r; ++i) {
+			auto row = m.row(i);
+			for (uint j = 0; j < c; ++j) {
+				REQUIRE(row[j] == m[i][j]);
+				row[j] = typename Mat::value_type();
+				REQUIRE(m[i][j] == typename Mat::value_type());
+			}
+		}
+	}
+};
+
+TEST_CASE("linalg/matrix: row returns a reference to the given row in the given matrix", "[linalg]")
+{
+	invoke_for_each_type<test_rows, TYPES>();
+}
+
+template<class Mat>
+struct test_cols
+{
+	void
+	operator()() const
+	{
+		auto m = create_mat<Mat>();
+
+		auto r = m.rows();
+		auto c = m.columns();
+
+		for (uint j = 0; j < c; ++j) {
+			auto col = m.column(j);
+			for (uint i = 0; i < r; ++i) {
+				REQUIRE(col[i] == m[i][j]);
+				col[i] = typename Mat::value_type();
+				REQUIRE(m[i][j] == typename Mat::value_type());
+			}
+		}
+	}
+};
+
+TEST_CASE("linalg/matrix: column returns a reference to the given row in the given matrix", "[linalg]")
+{
+	invoke_for_each_type<test_cols, TYPES>();
+}
+
+template<class Mat>
+struct test_transposed
+{
+	void
+	operator()() const
+	{
+		auto m = create_mat<Mat>();
+		auto tp = m.transposed();
+
+		auto r = m.rows();
+		auto c = m.columns();
+		REQUIRE(tp.rows() == c);
+		REQUIRE(tp.columns() == r);
+
+		for (uint i = 0; i < r; ++i) {
+			for (uint j = 0; j < c; ++j) {
+				REQUIRE(m[i][j] == tp[j][i]);
+			}
+		}
+	}
+};
+
+TEST_CASE("linalg/matrix: transposed returns the transpose of the given matrix", "[linalg]")
+{
+	invoke_for_each_type<test_transposed, TYPES>();
+}
